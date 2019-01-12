2026875c1a227c9e74b98c97bafb85cc443ae6eb
JarretYingling <44506154+JarretYingling@users.noreply.github.com>
2019-01-12 14:12:28 -0500

    commit local changes

---
+++ assets/javascript/history.js
@@ -0,0 +1,43 @@
+//muffin labs history api call, send individual arguments for month & day
+function getHistory(month, day) {
+
+    let endpoint = "https://cors-anywhere.herokuapp.com/https://history.muffinlabs.com/date/";
+    let queryURL = endpoint + month + "/" + day;
+    
+        return $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).done(function (response) {
+            response = JSON.parse(response);
+            historyObj.date = response.date;
+            historyObj.events = response.data.Events;
+            historyObj.births = response.data.Births;
+            historyObj.deaths = response.data.Deaths;
+        }).fail(function () {
+            throw err;
+        })
+    //return historyObj;
+};
+
+let historyObj = {
+    date: "",
+    events: [],
+    births: [],
+    deaths: []
+};
+
+//ready js
+$(document).ready(function () {
+
+    // pass dynamically from front end
+    let month = 8;
+    let day = 9;
+    
+    //historyObj = getHistory(month, day);
+    $.when(getHistory(month, day)).done(function () {
+        console.log(historyObj);
+    });
+    //historyDataObj = JSON.parse(historyDataObj);
+    
+    //end ready js
+});
